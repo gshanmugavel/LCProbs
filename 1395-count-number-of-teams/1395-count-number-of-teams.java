a52cc6ba50d2d0d4601df6976c220d8abb84d285
Giridharan Shanmugavel <giridharvel@gmail.com>
2022-07-05 19:06:08 +0530

    Time: 45 ms (30.93%), Space: 43.7 MB (33.30%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.java
+++ 1395-count-number-of-teams/1395-count-number-of-teams.java
@@ -1,23 +1,35 @@
 class Solution {
     public int numTeams(int[] rating) {
-        TreeSet<Integer>tset=new TreeSet<>();
-        TreeSet<Integer>set2=new TreeSet<>();
-        int total=0;
-        for(int i:rating){
-            tset.add(i);
-        }
-        for(int i=0;i<rating.length;i++){
-            tset.remove(rating[i]);
-            set2.add(rating[i]);
-            if(i!=0){
-                int lower=set2.subSet(Integer.MIN_VALUE,rating[i]).size();
-                int higher=tset.subSet(rating[i]+1,Integer.MAX_VALUE).size();
-                total+=(lower*higher);
-                int l=tset.subSet(Integer.MIN_VALUE,rating[i]).size();
-                int r=set2.subSet(rating[i]+1,Integer.MAX_VALUE).size();
-                total+=(l*r);
+		// DP matrix stores the total number of LIS pairs of length 1, 2 and 3 respectively
+        int[][] dp = new int[rating.length][4];
+        int count = 0;
+        dp[0][1] = 1;  // Every element has a 1 length LIS by default
+        
+        for(int i = 1; i < rating.length; i++) {
+            dp[i][1] = 1;
+            for(int j = 0; j < i; j++) {
+                if(rating[j] < rating[i]) {
+                    dp[i][2] += dp[j][1]; // Adding rating[i] makes the one length pairs ending at rating[j], two length pairs 
+                    dp[i][3] += dp[j][2]; // Adding rating[i] makes the two length pairs ending at rating[j], three length pairs
+                }
             }
+			
+			// As the number of soldiers are 3
+            count += dp[i][3];
         }
-        return total;
+		// In reverse order as mentioned in the statement
+        dp = new int[rating.length][4];
+        dp[dp.length - 1][1] = 1;
+        for(int i = dp.length - 2; i >= 0; i--) {
+            dp[i][1] = 1;
+            for(int j = dp.length - 1; j > i; j--) {
+                if(rating[j] < rating[i]) {
+                    dp[i][2] += dp[j][1];
+                    dp[i][3] += dp[j][2];
+                }
+            }
+            count += dp[i][3];
+        }
+        return count;
     }
 }
